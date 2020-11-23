d3bc13a7340a9102251ad6487b440b3ca65e4fa3
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2020-11-23 15:51:08 +0530

    Sixth Commit

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../hoc/Auxilliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 //Stateless components or DUMB
-const layout = (props) => (
-    <Aux>
-       <Toolbar />
-       <main className={classes.Content}>
-           {props.children}
-       </main>
-   </Aux>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
 
-export default layout;
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    render(){
+        return(
+        <Aux>
+            <Toolbar />
+            <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>
+        )
+    }
+} 
+
+export default Layout;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 import burgerLogo from '../../assets/images/burger-logo.png';
+import classes from './Logo.css';
 
 const logo = (props) => (
-    <div>
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={burgerLogo} alt="MyBurger" />
     </div>
 );
