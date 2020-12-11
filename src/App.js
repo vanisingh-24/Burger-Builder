ddba597ce084ab7a11a920f6044629707f8054b4
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2020-12-11 15:49:04 +0530

    Added authentication Form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import {Route, Switch} from 'react-router-dom';
-import Orders from './containers/Orders/Orders';
 
+import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 
@@ -16,6 +17,7 @@ class App extends Component {
           <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
           <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
---
+++ src/store/actions/auth.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+import * as actionTypes from './actionTypes';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (authData) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        authData: authData
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+
+export const auth = (email, password) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAQdvFpfQeHOdlUtQyD9s6uwLbXvtGDZCs', authData)
+          .then(response => {
+              console.log(response);
+              dispatch(authSuccess(response.data));
+          })
+          .catch(err => {
+              console.log(err);
+              dispatch(authFail(err));
+          });
+    };
+};
