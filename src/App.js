52090aa76d592235d25b571c119e52e162f7eee6
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2020-12-13 01:01:25 +0530

    Redirect User to checkout page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import {Route, Switch} from 'react-router-dom';
+import {connect} from 'react-redux';
 
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Logout from './containers/Auth/Logout/Logout';
+import * as actions from './store/actions/index';
 
 class App extends Component {
+  componentDidMount(){
+    this.props.onTryAutoSignup();
+  }
+  
   render() {
     return (
       <div>
@@ -28,4 +34,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -43,6 +43,12 @@ class Auth extends Component {
         isSignUp: true
     }
 
+    componentDidMount(){
+        if(!this.props.buildingBurger && this.props.authRedirectPath !== '/'){
+            this.props.onSetAuthRedirectPath();
+        }
+    }
+
     checkValidity(value, rules) {
         let isValid = true;
         if (!rules) {
@@ -133,7 +139,7 @@ class Auth extends Component {
 
         let authRedirect = null;
         if(this.props.isAuthenticated){
-            authRedirect = <Redirect to="/" />
+            authRedirect = <Redirect to={this.props.authRedirectPath} />
         }
 
         return (
@@ -154,13 +160,16 @@ const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
         error: state.auth.error,
-        isAuthenticated: state.auth.token !== null
+        isAuthenticated: state.auth.token !== null,
+        buildingBurger: state.burgerBuilder.building,
+        authRedirectPath: state.auth.authRedirectPath
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
+        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
     };
 };
 
