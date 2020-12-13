ae6059befdbf8081cabec6108b7b66281688f609
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2020-12-13 14:51:37 +0530

    Guarded our Routes

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -83,12 +83,12 @@ export const authCheckState = () => {
            dispatch(logout());
         }else{
             const expirationDate = new Date(localStorage.getItem('expirationDate'));
-            if(expirationDate > new Date()) {
+            if(expirationDate <= new Date()) {
                 dispatch(logout());
             }else {
                 const userId = localStorage.getItem('userId');
                 dispatch(authSuccess(token, userId));
-                dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
             }
 
         }
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -64,10 +64,11 @@ export const fetchOrderStart = () => {
     };
 };
 
-export const fetchOrders = (token) => {
+export const fetchOrders = (token, userId) => {
     return dispatch => {
         dispatch(fetchOrderStart());
-        axios.get('/orders.json?auth=' + token )
+        const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+        axios.get('/orders.json' + queryParams)
           .then(res => {
               const fetchedOrders = [];
               for(let key in res.data){
